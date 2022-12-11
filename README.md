42709502a85ddee312f2072200b1203edee0ab50
KJMAN678 <kj.fireman5@gmail.com>
2022-12-11 14:13:26 +0900

    チュートリアル4.8までを反映

--- README.md
+++ README.md
@@ -14,6 +14,35 @@
 - nvm 0.39.2
 - firebase-tools 11.17.0
 
+### メモ
+
+- 4.5 のテストは 下記で行った
+  wasm-pack test --chrome --headless
+
+- 4.7
+
+  - index.js の
+
+```rust
+const playPauseButton = document.getElementById("play-pause");
+```
+
+の下に
+
+```rust
+playPauseButton.textContent = "⏸";
+```
+
+を追加して初期画面にボタンが表示されるようにした
+
+- 4.8 に
+  - index.html と同じフォルダに index.css を追加
+  - index.html の <header> に、下記を追加
+
+```css
+<link rel="stylesheet" type="text/css" href="index.css">
+```
+
 ### 構成ファイル
 
 - 更新したファイルのみアップロードしている
@@ -23,5 +52,5 @@
 - [Rust + WebAssembly のチュートリアル](https://rustwasm.github.io/docs/book/introduction.html)
 - [Error: error:0308010C:digital envelope routines::unsupported 対策](https://zenn.dev/pontagon333/articles/26c89cbc14e81f)
 - [JavaScript のレンダリングにスリープ処理を入れる](https://www.wakuwakubank.com/posts/695-javascript-timer-sleep/)
-- [Firebase へのデプロイ](https://qiita.com/namn1125/items/8ffa0f87fa03f59f3acc)
+- [Firebase へのデプロイ](https://qiita.com/namn1125/items/8ffa0f87fa03f59f3acc#4-firebase%E3%81%A7%E3%83%87%E3%83%97%E3%83%AD%E3%82%A4)
 - [nvm で node のバージョン切り替え](https://maku77.github.io/nodejs/env/nvm)
--- www/index.js
+++ www/index.js
@@ -1,16 +1,191 @@
-import { Universe } from "rust-wasm";
+import { Cell, Universe } from "rust-wasm";
+import { memory } from "rust-wasm/rust_wasm_bg";
 
-const pre = document.getElementById("rust-wasm-canvas");
+const CELL_SIZE = 5;
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
 const universe = Universe.new();
-const renderLoop = callback => {
+const width = universe.width();
+const height = universe.height();
 
-	if (Math.round(callback / 10) * 10 % 100 == 0) {
-	    pre.textContent = universe.render();
+const canvas = document.getElementById("rust-wasm-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+        this.fps.textContent = `
+        Frames per Seconds:
+        latest = ${Math.round(fps)}
+        avg of last 100 = ${Math.round(mean)}
+        min of last 100 = ${Math.round(min)}
+        max of last 100 = ${Math.round(max)}
+        `.trim();
+    }
+};
+
+
+let animationId = null;
+
+const renderLoop = () => {
+
+    fps.render();
+
+    // debugger;
+
+    for (let i = 0; i < 9; i++) {
         universe.tick();
-	}
+    }
+
+    drawGrid();
+    drawCells();
+
+    animationId = requestAnimationFrame(renderLoop);
+};
+
+const isPaused = () => {
+    return animationId == null;
+};
+
+const playPauseButton = document.getElementById("play-pause");
+playPauseButton.textContent = "⏸";
+
+const play = () => {
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+};
+
+const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+        play();
+    } else {
+        pause();
+    }
+});
 
 
-    requestAnimationFrame(renderLoop);
-}
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
 
+    for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+
+    for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    ctx.stroke();
+};
+
+const getIndex = (row, column) => {
+    return row * width + column;
+};
+
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    ctx.fillStyle = ALIVE_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if (cells[idx] !== Cell.Alive) {
+                continue;
+            }
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.fillStyle = DEAD_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if (cells[idx] !== Cell.Dead) {
+                continue;
+            }
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+
+    ctx.stroke();
+};
+
+
+canvas.addEventListener("click", event => {
+    const boundingRect = canvas.getBoundingClientRect();
+
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+    universe.toggle_cell(row, col);
+
+    drawGrid();
+    drawCells();
+});
+
+
+
+drawGrid();
+drawCells();
 requestAnimationFrame(renderLoop);
